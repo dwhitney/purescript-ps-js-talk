a5c62353b5f4baa14291af96357e719860d16cd5
Dustin Whitney <dustin.whitney@gmail.com>
2018-08-06 21:03:31 -0400

    full app in javascript

--- js/SampleChart.js
+++ js/SampleChart.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 import createChart from "./charting/createChart";
 
-type Props = {
-  type: string
-};
-
 class SampleChart extends Component<Props> {
   constructor(props: Props) {
     super(props);
--- js/index.js
+++ js/index.js
@@ -1,31 +1,29 @@
-/* @flow */
-
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 
+import App from "./App";
 import SampleChart from "./SampleChart";
 
-const app = (
-  <Fragment>
-    <SampleChart type="punch_card" />
-    <SampleChart type="scatter" />
-    <SampleChart type="pie" />
-    <SampleChart type="candlestick" />
-    <SampleChart type="stacked_bar" />
-    <SampleChart type="bar_range" />
-    <SampleChart type="layout" />
-    <SampleChart type="line" />
-    <SampleChart type="stacked_line_area" />
-    <SampleChart type="step_line" />
-    <SampleChart type="theme_river" />
-    <SampleChart type="funnel" />
-    <SampleChart type="radar" />
-    <SampleChart type="sunburst" />
-    <SampleChart type="graph" />
-    <SampleChart type="punch_card" />
-    <SampleChart type="stock_fan" />
-  </Fragment>
-);
+const app = <App charts={["punch_card", "scatter"]} />;
 
 // $FlowFixMe
-ReactDOM.render(app, document.getElementById("main"));
+ReactDOM.render(app, document.getElementById("app"));
+
+/*
+<SampleChart type="scatter" />
+<SampleChart type="pie" />
+<SampleChart type="candlestick" />
+<SampleChart type="stacked_bar" />
+<SampleChart type="bar_range" />
+<SampleChart type="layout" />
+<SampleChart type="line" />
+<SampleChart type="stacked_line_area" />
+<SampleChart type="step_line" />
+<SampleChart type="theme_river" />
+<SampleChart type="funnel" />
+<SampleChart type="radar" />
+<SampleChart type="sunburst" />
+<SampleChart type="graph" />
+<SampleChart type="punch_card" />
+<SampleChart type="stock_fan" />
+*/
